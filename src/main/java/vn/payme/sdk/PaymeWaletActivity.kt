c0f26254fe1a153f741c11203a353caacb3bb2dc
hieu lai <hieulv95.hubt@gmail.com>
2020-09-18 15:50:39 +0700

    fix bug crash

--- src/main/java/vn/payme/sdk/PaymeWaletActivity.kt
+++ src/main/java/vn/payme/sdk/PaymeWaletActivity.kt
@@ -3,7 +3,6 @@ package vn.payme.sdk
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.webkit.WebView
-import com.payme.sdk.walet.R
 
 class PaymeWaletActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
