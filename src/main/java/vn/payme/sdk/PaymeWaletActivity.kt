70a4be0005fb3167215ef3606993d4baa1fc09c3
hieu lai <hieulv95.hubt@gmail.com>
2020-09-24 17:32:15 +0700

    them oject giao tiep vs web

--- src/main/java/vn/payme/sdk/PaymeWaletActivity.kt
+++ src/main/java/vn/payme/sdk/PaymeWaletActivity.kt
@@ -1,17 +1,51 @@
 package vn.payme.sdk
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebMessage
 import android.webkit.WebView
+import vn.payme.sdk.model.JsObject
+
 
 internal class PaymeWaletActivity : AppCompatActivity() {
+    @SuppressLint("JavascriptInterface")
     override fun onCreate(savedInstanceState: Bundle?) {
+        val html :String = "<!DOCTYPE html><html><body>\n" +
+                "      <button onclick=\"onClick()\">Click me</button>\n" +
+                "      <script>\n" +
+                "      function onClick() {\n" +
+                "       window.injectedObject.onError('ádasdasdd')" +
+                "      }\n" +
+                "      </script>\n" +
+                "      </body></html>\n"
         super.onCreate(savedInstanceState)
         setContentView(R.layout.webview_activity)
         val myWebView: WebView = findViewById(R.id.webview)
-    myWebView.settings.javaScriptEnabled = true
-    myWebView.loadUrl("https://sdk.payme.com.vn/?deviceId=${PayME.deviceId}")
+        myWebView.settings.javaScriptEnabled = true
+        val jsObject : JsObject = JsObject()
+
+        myWebView.addJavascriptInterface(jsObject,"injectedObject")
+
+//        myWebView.loadData(html,"text/html", "UTF-8")
+
+        myWebView.loadUrl("https://sbx-sdk.payme.com.vn/?deviceId=${PayME.deviceId}")
+//        val webMessage : WebMessage = WebMessage()
+
+//        val webviewInterface : JsObject = JsObject()
+
+//        myWebView.addJavascriptInterface(webviewInterface,"")
+//        webviewInterface.onSuccess()
+
+
+
+
+//        myWebView.createWebMessageChannel()
+
+//        myWebView.postWebMessage()
 
     }
+
+
 
 }
--- src/main/java/vn/payme/sdk/model/JsObject.kt
+++ src/main/java/vn/payme/sdk/model/JsObject.kt
@@ -1,4 +1,18 @@
 package vn.payme.sdk.model
 
-class JsObject {
+import android.webkit.JavascriptInterface
+import org.json.JSONObject
+
+
+public class JsObject {
+    @JavascriptInterface
+    public  fun onSuccess(jsonObject: JSONObject){
+
+    }
+
+    @JavascriptInterface
+    public  fun onError(string: String){
+        println("adasadasdadsasasddd")
+
+    }
 }
