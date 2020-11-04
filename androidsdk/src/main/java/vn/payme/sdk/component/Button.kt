aaeb16bbc21ae56539c38872d02b5eb881e1485f
hieu lai <hieulv95.hubt@gmail.com>
2020-11-04 17:41:03 +0700

    Done loading

---
+++ androidsdk/src/main/java/vn/payme/sdk/component/Button.kt
@@ -0,0 +1,4 @@
+package vn.payme.sdk.component
+
+class Button {
+}
--- androidsdk/src/main/java/vn/payme/sdk/component/ProgressButton.kt
+++
@@ -1,9 +0,0 @@
-package vn.payme.sdk.component
-
-import android.content.Context
-import android.view.View
-
-class ProgressButton {
-
-    constructor(context: Context,view: View)
-}
