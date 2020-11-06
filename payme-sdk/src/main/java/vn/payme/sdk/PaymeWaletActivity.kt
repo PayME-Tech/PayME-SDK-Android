efdc9c7a9e8bad4c006897aa4f80f70ac266d1ed
hieu lai <hieulv95.hubt@gmail.com>
2020-11-06 14:42:28 +0700

    fix open browser

--- payme-sdk/src/main/java/vn/payme/sdk/PaymeWaletActivity.kt
+++ payme-sdk/src/main/java/vn/payme/sdk/PaymeWaletActivity.kt
@@ -4,6 +4,8 @@ import android.Manifest
 import android.annotation.TargetApi
 import android.content.pm.PackageManager
 import android.graphics.Color
+import android.net.Uri
+import android.net.http.SslError
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
@@ -12,7 +14,6 @@ import android.view.WindowManager
 import android.webkit.*
 import androidx.appcompat.app.AppCompatActivity
 import org.json.JSONObject
-import vn.payme.sdk.model.Action
 import vn.payme.sdk.model.Env
 import vn.payme.sdk.model.JsObject
 import java.net.URLEncoder
@@ -46,9 +47,11 @@ internal class PaymeWaletActivity : AppCompatActivity() {
                 "      </body></html>\n"
         super.onCreate(savedInstanceState)
 
+
+
         window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
         window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
-        window.statusBarColor = Color.TRANSPARENT
+        window.statusBarColor = Color.DKGRAY
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
         setContentView(R.layout.webview_activity)
         var statusBarHeight = 0
@@ -57,18 +60,36 @@ internal class PaymeWaletActivity : AppCompatActivity() {
             statusBarHeight = resources.getDimensionPixelSize(resourceId)
         }
         val myWebView: WebView = findViewById(R.id.webview)
-        myWebView.settings.javaScriptEnabled = true
-        myWebView.settings.mediaPlaybackRequiresUserGesture = false
-        myWebView.setWebViewClient(object : WebViewClient() {
-            override fun shouldOverrideUrlLoading(view: WebView, url: String?): Boolean {
-                myWebView.loadUrl(url)
-                return true
+        val cookieManager = CookieManager.getInstance()
+        cookieManager.setAcceptCookie(true)
 
+
+        myWebView.setWebViewClient(object : WebViewClient() {
+            override fun onPageFinished(view: WebView?, url: String?) {
+
+                super.onPageFinished(view, url)
             }
         })
-        CookieManager.getInstance().setAcceptThirdPartyCookies(myWebView, true);
+
+        val webSettings: WebSettings = myWebView.getSettings()
+        webSettings.javaScriptEnabled = true
+        webSettings.setAppCacheEnabled(true)
+        webSettings.layoutAlgorithm = WebSettings.LayoutAlgorithm.NORMAL
+        webSettings.javaScriptCanOpenWindowsAutomatically = true
+        webSettings.mediaPlaybackRequiresUserGesture = false
+        webSettings.setSupportMultipleWindows(true)
+        webSettings.setGeolocationEnabled(true)
+        webSettings.domStorageEnabled = true
+        webSettings.databaseEnabled = true
+        webSettings.setGeolocationEnabled(true)
+        webSettings.setGeolocationDatabasePath(filesDir.path)
+
+        webSettings.loadWithOverviewMode = true
+        webSettings.allowFileAccess = true
+        webSettings.cacheMode = WebSettings.LOAD_CACHE_ELSE_NETWORK
 
 
+//        setContentView(myWebView)
         myWebView.setWebChromeClient(object : WebChromeClient() {
             override fun onConsoleMessage(consoleMessage: ConsoleMessage?): Boolean {
                 Log.d(
@@ -78,6 +99,7 @@ internal class PaymeWaletActivity : AppCompatActivity() {
                 );
                 return super.onConsoleMessage(consoleMessage)
             }
+
 
             // Grant permissions for cam
             @TargetApi(Build.VERSION_CODES.LOLLIPOP)
@@ -117,20 +139,17 @@ internal class PaymeWaletActivity : AppCompatActivity() {
                       configColor: ['${PayME.configColor?.get(0)}', '${PayME.configColor?.get(1)}']
                     }"""
         )
+
         val encode: String = URLEncoder.encode(data.toString(), "utf-8")
-        if (PayME.action == Action.TEST) {
-            myWebView.loadUrl("https://sbx-sdk.payme.com.vn/test")
+//        cookieManager.setCookie("https://sbx-sdk.payme.com.vn/active/${encode}","$cookieKey=$cookieValue")
+        cookieManager.setAcceptThirdPartyCookies(myWebView, true)
+
+
+        if (PayME.env === Env.SANDBOX) {
+            myWebView.loadUrl("https://sbx-sdk.payme.com.vn/active/${encode}")
 
         } else {
-            if (PayME.env === Env.SANDBOX) {
-//            myWebView.loadUrl("https://sbx-sdk.payme.com.vn/result?success=true&trans_id=0792609928&amount=10000")
-                myWebView.loadUrl("https://sbx-sdk.payme.com.vn/active/${encode}")
-//                myWebView.loadUrl("https://vi.webcamtests.com")
-
-            } else {
-                myWebView.loadUrl("https://sdk.payme.com.vn/active/${encode}")
-
-            }
+            myWebView.loadUrl("https://sdk.payme.com.vn/active/${encode}")
         }
 
 
