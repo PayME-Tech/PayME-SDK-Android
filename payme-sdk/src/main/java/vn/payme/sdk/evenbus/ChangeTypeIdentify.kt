4c77708050fe23550ca6b4d72243fc0a23c01652
hieu lai <hieulv95.hubt@gmail.com>
2020-12-16 11:52:46 +0700

    done scan qr

---
+++ payme-sdk/src/main/java/vn/payme/sdk/evenbus/ChangeTypeIdentify.kt
@@ -0,0 +1,10 @@
+package vn.payme.sdk.evenbus
+
+import vn.payme.sdk.model.TypeCallBack
+
+class ChangeTyIdentify {
+    var value: String?
+    constructor(value: String) {
+        this.value = value
+    }
+}
