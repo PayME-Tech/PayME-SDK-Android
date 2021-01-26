6836b887ec9df45646c346073829c60d06e1d7e2
hieu lai <hieulv95.hubt@gmail.com>
2021-01-26 15:14:31 +0700

    update font

--- payme-sdk/src/main/java/vn/payme/sdk/model/ClientInfo.kt
+++ payme-sdk/src/main/java/vn/payme/sdk/model/ClientInfo.kt
@@ -10,7 +10,7 @@ import java.lang.reflect.Method
 
 class ClientInfo {
     var appVersion: String = ""
-    var sdkVerSion: String = "0.2.7"
+    var sdkVerSion: String = "0.2.8"
     var appPackageName: String? = ""
     var deviceId: String? = ""
     fun getDeviceName(): String? {
