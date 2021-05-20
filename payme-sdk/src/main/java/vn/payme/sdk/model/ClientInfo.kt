0b3cd0c86bfb36ae1485cb0eea3f99dac42a92e7
hieulv <hieulv@payme.vn>
2021-05-20 14:04:44 +0700

    up 0.4.7

--- payme-sdk/src/main/java/vn/payme/sdk/model/ClientInfo.kt
+++ payme-sdk/src/main/java/vn/payme/sdk/model/ClientInfo.kt
@@ -10,7 +10,7 @@ import java.lang.reflect.Method
 
 class ClientInfo {
     var appVersion: String = ""
-    var sdkVerSion: String = "0.4.6"
+    var sdkVerSion: String = "0.4.7"
     var appPackageName: String? = ""
     var deviceId: String? = ""
     fun getDeviceName(): String? {
