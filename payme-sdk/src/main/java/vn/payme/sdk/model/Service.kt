35f7fc0f8fa57d5c50a0a26a4bec6eadcabfa0db
dattm <dattm@edoctor.vn>
2022-05-26 10:17:00 +0700

    Remove unsuse variables

--- payme-sdk/src/main/java/vn/payme/sdk/model/Service.kt
+++ payme-sdk/src/main/java/vn/payme/sdk/model/Service.kt
@@ -3,13 +3,8 @@ package vn.payme.sdk.model
 class Service {
     lateinit var code: String
     lateinit var description: String
-    var disable: Boolean = false
-    var enable: Boolean = false
     constructor(code: String,description: String){
         this.code = code
         this.description = description
-
-
     }
-
 }
